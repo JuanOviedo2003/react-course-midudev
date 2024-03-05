6de8c941105586cbca0466f4a3a9723f048930cd
JuanOviedo2003 <oviedodiazjuanjose@gmail.com>
2024-03-04 22:36:25 -0500

    Usando objetos para la creación de componentes y JS para agregar los datos de los objetos a los componentes

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -2,12 +2,44 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
+const users = [
+    {
+        userName: 'midudev',
+        name: 'Miguel Ángel Duran',
+        isFollowing: true
+    },
+    {
+        userName: 'JuanOviedo2003',
+        name: 'Juan Jose Oviedo',
+        isFollowing: false
+    },
+    {
+        userName: 'CarlosRivera247',
+        name: 'Carlos Felipe Rivera',
+        isFollowing: true
+    }
+]
+
 export function App (){
     return(
         <section className='App'> 
-            <TwitterFollowCard userName="JuanOviedo2003" initialIsFollowing={true}>
-                Juan Jose Oviedo
-            </TwitterFollowCard>
+            {
+                users.map(user => {
+                    const {userName, name, isFollowing} = user
+                    return(
+                        <TwitterFollowCard
+                            key={userName} 
+                            // * Se agrega una key para diferenciar este elemento de los demas en el DOM
+                            // Normalmente se pone como key algo que haga que ese elemento sea unico
+                            // * EN ESTE CASO EL userName
+                            userName={userName}
+                            initialIsFollowing={isFollowing}
+                        >
+                            {name}
+                        </TwitterFollowCard>
+                    )
+                })
+            }
         </section>
     )
 }
